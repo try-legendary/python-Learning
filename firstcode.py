f08cfceaebe2018a55439f27e4e5f18283210447
Sk. Rakinul Haque <rakintrying@gmail.com>
2026-01-15 15:18:20 +0600

    Delete firstcode.py

--- firstcode.py
+++
@@ -1,8 +0,0 @@
-#WAP to input 2 int numbers, a and b.
-#Print True if a is greater than or equal to b. If not print False.
-
-a=float(input("Enter 1st numeber:"));
-b=float(input("Enter 2nd number:"));
-
-
-print(a>=b);
