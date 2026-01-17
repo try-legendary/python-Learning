3a0559d6dabd98d9a0b3daae759f5c36a2c4ac05
Sk. Rakinul Haque <rakintrying@gmail.com>
2026-01-17 18:53:27 +0600

    Unpack a list [1,2,3,4,5] into first, second, rest

--- q14.py
+++ q14.py
@@ -1,8 +1,7 @@
-a={1,2,3,4,5};
-len=len(a);
-result="";
-for x in range(0,len-1,1):
- char=a[x];
- result=result+char;
-
-print(result)
+# Unpack a list [1,2,3,4,5] into first, second, rest
+a=[1,2,3,4,5];
+print("1st",a[0]);
+print("2nd",a[1]);
+print("3rd",a[2]);
+print("4th",a[3]);
+print("5th",a[4]);
