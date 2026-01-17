79dc017bbc99bd064eafb9490bf10db645fc20b5
Sk. Rakinul Haque <rakintrying@gmail.com>
2026-01-17 18:39:43 +0600

    python documentation

---
+++ q12.py
@@ -0,0 +1,8 @@
+##Swap three variables in circular rotation (a->b, b->c, c->a)
+
+a, b, c=input("enter 3 variables with space:").split();
+
+a,b,c=c,b,a;
+
+
+print(a, b, c)
---
+++ q14.py
@@ -0,0 +1,8 @@
+a={1,2,3,4,5};
+len=len(a);
+result="";
+for x in range(0,len-1,1):
+ char=a[x];
+ result=result+char;
+
+print(result)
